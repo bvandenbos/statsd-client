2b756baee335fec1d8d4708f6293a71e50e8fa9e
Raffael Schmid <raf@nine.ch>
2012-06-08 11:26:34 +0200

    add test and code for gauge values

--- lib/statsd.rb
+++ lib/statsd.rb
@@ -2,22 +2,22 @@ require 'socket'
 require 'resolv'
 
 class Statsd
-  
+
   Version = '0.0.6'
-  
+
   class << self
-    
+
     attr_accessor :host, :port
-    
+
     def host_ip_addr
       @host_ip_addr ||= Resolv.getaddress(host)
     end
-    
+
     def host=(h)
       @host_ip_addr = nil
       @host = h
     end
-    
+
     # +stat+ to log timing for
     # +time+ is the time to log in ms
     def timing(stat, time = nil, sample_rate = 1)
@@ -28,29 +28,33 @@ class Statsd
       end
       send_stats("#{stat}:#{time}|ms", sample_rate)
     end
-    
+
+    def gauge(stat, value, sample_rate = 1)
+      send_stats("#{stat}:#{value}|g", sample_rate)
+    end
+
     # +stats+ can be a string or an array of strings
     def increment(stats, sample_rate = 1)
       update_counter stats, 1, sample_rate
     end
-    
+
     # +stats+ can be a string or an array of strings
     def decrement(stats, sample_rate = 1)
       update_counter stats, -1, sample_rate
     end
-    
+
     # +stats+ can be a string or array of strings
     def update_counter(stats, delta = 1, sample_rate = 1)
       stats = Array(stats)
       send_stats(stats.map { |s| "#{s}:#{delta}|c" }, sample_rate)
     end
-    
+
     private
-    
+
     def send_stats(data, sample_rate = 1)
       data = Array(data)
       sampled_data = []
-      
+
       # Apply sample rate if less than one
       if sample_rate < 1
         data.each do |d|
@@ -60,11 +64,11 @@ class Statsd
         end
         data = sampled_data
       end
-      
+
       return if data.empty?
-      
+
       raise "host and port must be set" unless host && port
-      
+
       begin
         sock = UDPSocket.new
         data.each do |d|
@@ -76,7 +80,7 @@ class Statsd
       end
       true
     end
-    
+
   end
-  
+
 end
